8236a510f8a006140124c19f1e40f2349fa0cd6b
Edison Lim <babyedison@gmail.com>
2022-02-27 18:43:28 +0800

    C 09 100%

--- C Piscine C 09/ex00/ft_strcmp.c
+++ C Piscine C 09/ex00/ft_strcmp.c
@@ -17,10 +17,8 @@ int	ft_strcmp(char *s1, char *s2)
 	counter = 0;
 	while (s1[counter] != 0 || s2[counter] != 0)
 	{
-		if (s1[counter] < s2[counter])
-			return (-1);
-		else if (s1[counter] > s2[counter])
-			return (1);
+		if (s1[counter] != s2[counter])
+			return (s1[counter] - s2[counter]);
 		counter++;
 	}
 	return (0);
