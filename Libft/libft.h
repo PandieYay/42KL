7dd9df231138fc7b51b06ec5feacec5fbb7aaa6d
Edison Lim <babyedison@gmail.com>
2022-04-07 21:00:52 +0800

    lstsize done

--- Libft/libft.h
+++ Libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edlim <edlim@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: edlim <edlim@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 13:15:43 by edlim             #+#    #+#             */
-/*   Updated: 2022/04/04 13:15:44 by edlim            ###   ########.fr       */
+/*   Updated: 2022/04/07 20:56:50 by edlim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,7 @@ typedef struct s_list
 
 t_list	*ft_lstnew(void *content);
 void ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
 
 #endif
