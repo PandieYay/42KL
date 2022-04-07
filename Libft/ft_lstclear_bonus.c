ef9f3ba3460c4ba4bf032b764104eb2e545c392d
Edison Lim <babyedison@gmail.com>
2022-04-08 00:22:08 +0800

    Last to delone done

---
+++ Libft/ft_lstclear_bonus.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+
+// void ft_lstclear(t_list **lst, void (*del)(void*))
+// {
+//     t_list *head;
+
+//     head = *lst;
+//     del(lst->content);
+//     free(lst);
+// }
---
+++ Libft/ft_lstlast_bonus.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+
+t_list *ft_lstlast(t_list *lst)
+{
+    if (!lst)
+		return (NULL);
+    while (lst->next != 0)
+		lst = lst->next;
+	return (lst);
+}
